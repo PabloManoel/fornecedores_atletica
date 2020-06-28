9143937f587f42ed563c906f660a2144085eda60
Pablo Manoel <pablo.manoel@mobicare.com.br>
2020-06-28 17:01:17 -0300

    removing duplicated filter component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import './App.css';
-import FilterSearch from './components/FilterSearch';
 import ListItems from "./components/ProductsList";
 import SideBar from './components/SideBar';
 
@@ -9,7 +8,6 @@ function App() {
   return (
     <div className="App">
       <SideBar></SideBar>
-      <FilterSearch></FilterSearch>
       <ListItems></ListItems>
       
     </div>
