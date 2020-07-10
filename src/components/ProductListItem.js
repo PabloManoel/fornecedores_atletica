6352014a5124d35e1edcbb704e95a098b09c6718
Pablo Manoel <pablo.manoel@mobicare.com.br>
2020-07-10 12:07:46 -0300

    - removendo funcoes não utilizadas

--- src/components/ProductListItem.js
+++ src/components/ProductListItem.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 
 function ProductListItem(props) {
 
@@ -40,8 +40,8 @@ function ProductListItem(props) {
 	)
 }
 
-function consoleChange(event) {
-	console.log("was changed");
-}
+// function consoleChange(event) {
+// 	console.log("was changed");
+// }
 export default ProductListItem;
 
